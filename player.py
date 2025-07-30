d532731d7f5f40fb1d83d78796b96c1ed4dcc58c
nazabu <nazrievabubakr2007@gmail.com>
2025-07-30 14:50:52 -0600

    Complete Player file

---
+++ player.py
@@ -0,0 +1,19 @@
+def player_move(board, symbol):
+    is_valid_input = False
+    while not is_valid_input:
+        cell = input("Enter row (R) and column (C) index (0-2) together like \"RC\": ")
+        if board[cell[0], cell[1]] == " " and input_checker(cell):
+            board[cell[0], cell[1]] = symbol
+            is_valid_input = True
+        else:
+            print("Invalid input. Try again.")
+
+def input_checker(input):
+    if len(input) != 2:
+        return False
+
+    index = [0,1,2]
+    if input[0] not in index or input[1] not in index:
+        return False
+
+    return True
